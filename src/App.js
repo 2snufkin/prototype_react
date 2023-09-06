ae4d9b942e5987dcf0e9cba4fa19eec63e933824
Pini <pinhas.zrouya@sesan.fr>
2023-09-06 19:31:11 +0200

    adding context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Footer, Header} from "./components/layout";
 function App() {
   return (
       <div className="flex flex-col min-h-screen ">
+          {/*TODO : the burger button hide content*/}
           <Header />
           <main className="flex-grow pt-16">
               <AllRoutes />
---
+++ src/context/reducers/cartReducer.js
@@ -0,0 +1,13 @@
+/*
+  File: reducer.js
+  Description: This file defines the reducer function used in conjunction with the Context API for state management.
+
+  Role:
+  - Reducer functions specify how the application's state changes in response to dispatched actions.
+  - It helps maintain a predictable and centralized way to update the global state.
+
+  What to Expect:
+  - Definition of the initial state and data structure.
+  - A switch statement that handles different action types and modifies the state accordingly.
+  - Export of the reducer function for use with the Context API.
+*/
